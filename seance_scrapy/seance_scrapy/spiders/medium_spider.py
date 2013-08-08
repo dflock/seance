bc2a518bd6a19752ac5fb462b807d9bab5b12f98
Duncan Lock <duncan.lock@gmail.com>
2013-08-07 17:27:50 -0700

    Switched over to using sitemaps to scrape, working better

--- seance_scrapy/seance_scrapy/spiders/medium_spider.py
+++ seance_scrapy/seance_scrapy/spiders/medium_spider.py
@@ -2,31 +2,36 @@
 # -*- coding: utf-8 -*-
 
 from scrapy.selector import HtmlXPathSelector
-from scrapy.spider import BaseSpider
+# from scrapy.selector import XmlXPathSelector
+# from scrapy.spider import BaseSpider
+
+from scrapy.contrib.spiders import SitemapSpider
 
 from seance_scrapy.items import MediumArticle
 
-class MediumSpider(BaseSpider):
+class MediumSpider(SitemapSpider):
     name = "medium"
     allowed_domains = ["medium.com"]
-    start_urls = [
-        "https://medium.com/"
-    ]
+    start_urls = ["https://medium.com/static/posts.xml"]
+    # sitemap_urls = ['https://medium.com/static/posts.xml']
+    sitemap_urls = ['https://medium.com/static/posts/posts-2013-07-30.xml']
+
+    items = []
 
     def parse(self, response):
+        # Load posts.xml and churn through it, one article at a time, collecting and populating MediumArticle items as we find them.
         hxs = HtmlXPathSelector(response)
-        sites = hxs.select('//article')
-        items = []
-        for site in sites:
-            item = MediumArticle()
 
-            item['title'] = site.select('.//h3/a/text()').extract()
-            item['url'] = site.select('.//h3/a/@href').extract()
-            item['desc'] = site.select('.//a[@class="post-item-snippet"]/p/text()').extract()
+        item = MediumArticle()
 
-            article_meta = site.select('.//ul[@class="post-item-meta"]')
-            item['author_name'] = article_meta.select('.//li[1]/a[1]/text()').extract()
-            item['min_read'] = article_meta.select('.//li[2]//span[@class="reading-time"]/text()').re('\d+')
-            items.append(item)
+        item['url'] = response.url
+        item['title'] = hxs.select('//h1[@class="post-title"]/text()').extract()
+        item['desc'] = hxs.select('//meta[@name="description"]/text()').extract()
 
-        return items
+        article_meta = hxs.select('//ul[@class="post-meta"]')
+        item['author_name'] = article_meta.select('.//li[1]/a[1]/text()').extract()
+        item['min_read'] = article_meta.select('.//li[2]//span[@class="reading-time"]/text()').re('\d+')
+
+        self.items.append(item)
+
+        return self.items
